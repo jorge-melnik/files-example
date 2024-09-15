71d5cbb24b2618dce74af50554c23fe242f1ca41
Jorge Melnik <jmelnik19@gmail.com>
2024-09-15 20:56:42 -0300

    redireccionar a localhost

--- backend/src/routes/get/index.ts
+++ backend/src/routes/get/index.ts
@@ -3,7 +3,8 @@ import { FastifyPluginAsync } from "fastify";
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get("/", async function (request, reply) {
     const queryString = request.query;
-    return queryString;
+    console.log({ queryString });
+    reply.redirect("http://localhost");
   });
 };
 
